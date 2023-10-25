583e97a3be298d07a0486a2a8dfe791ea2e39df9
xBridge1 <44344058+xBridge1@users.noreply.github.com>
2023-10-25 09:05:51 -0300

    Update random game picker.sql

--- random game picker.sql
+++ random game picker.sql
@@ -1,12 +1,17 @@
--- TABLE
+
 CREATE TABLE Games(
   id int(2),
   Jogo varchar(50),
   Genero varchar(50));
  
--- INDEX
- 
--- TRIGGER
- 
--- VIEW
- 
+INSERT INTO Games
+VALUES
+('1','Grand Theft Auto V','Mundo Aberto'),
+('2','Need For Speed Underground','Mundo Aberto-Corrida'),
+('3','Grand Theft Auto IV','Mundo Aberto'),
+('4','Madden NFL 23','Football Americano');
+
+SELECT jogo FROM Games
+ORDER BY RANDOM()
+LIMIT 1
+
